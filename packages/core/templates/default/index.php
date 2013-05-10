73b02b45ed418d80cbc244259a935c4374d2f144
Randolph Taylor <mmindking@gmail.com>
2013-05-10 02:55:12 -0400

    Users,Subjects,SubjectSources,SubjectTypes
    http://localhost/archon/?p=core/subjects&apilogin=admin&apipassword=admin&substart=1
    This URL Returns subjects in batches of 100 . Substart decides the
    beginning marker. If the substart is 1 then the group returned is  1-100
    (0  is handles as 1 as well) , the next substart should be incremented
    by 100.101 -201 would be the next range.
    
    http://localhost/archon/?p=core/users&apilogin=admin&apipassword=admin
    This URL Returns a list of users the permissions array for each object
    contains the relationship between users the permissions and their
    repositories .
    
    http://localhost/archon/?p=core/subjectsources&apilogin=admin&apipassword=admin
    This URL Returns a list of Subject sources
    
    http://localhost/archon/?p=core/subjecttypes&apilogin=admin&apipassword=admin
    This URL Returns a list subjecttypes.

--- packages/core/templates/default/index.php
+++ packages/core/templates/default/index.php
@@ -5,6 +5,10 @@ $TemplateIndex['Register'] = 'register.inc.php';
 $TemplateIndex['Login'] = 'login.inc.php';
 $TemplateIndex['EditProfile'] = 'edit-profile.inc.php';
 $TemplateIndex['Repositorylist'] = 'repository-list.inc.php';
+$TemplateIndex['Userlist'] = 'user-list.inc.php';
+$TemplateIndex['Subjectlist'] = 'subject-list.inc.php';
+$TemplateIndex['SubjectSourcelist'] = 'subjectsource-list.inc.php';
+$TemplateIndex['SubjectTypeslist'] = 'subjecttype-list.inc.php';
 $TemplateIndex['SearchTitle'] = 'search-title.inc.php';
 
 // Form sub-templates
