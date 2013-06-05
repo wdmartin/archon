bc5b1da0e08214024195570d1bcae14bcca0e8c7
chrisprom <chris.prom@gmail.com>
2013-06-05 16:42:03 -0500

    add getAllSubjectsforJSON function and cleanup JSON formatting for subjects

--- packages/core/templates/default/subject-list.inc.php
+++ packages/core/templates/default/subject-list.inc.php
@@ -5,27 +5,29 @@ isset($_ARCHON) or die();
 //echo print_r($_REQUEST) ;
 //echo print_r($_ARCHON);
 
-
 // echo print_r($arrCountries);
-
 
 if ($_REQUEST['apilogin'] && $_REQUEST['apipassword']) {
     if (!$_ARCHON->Security->verifyCredentials($_REQUEST['apilogin'], $_REQUEST['apipassword'])) {
         $_ARCHON->declareError("Authentication Failed");
     }
-    if (!$_ARCHON->Error) {
-//Handles the zero condition
-        $start = ( $_REQUEST['substart'] < 1 ? 1: $_REQUEST['substart']);
-
-// pulls Batches of 100 across
-        echo json_encode(array_slice($_ARCHON->getAllSubjects(),$start-1,100));
-    } else {
+    	if (!$_ARCHON->Error) {		
+			if ($_REQUEST['batch_start']){
+			//Handles the zero condition
+        	$start = ( $_REQUEST['batch_start'] < 1 ? 1: $_REQUEST['batch_start']);
+			// pulls Batches of 100 across
+        	echo json_encode(array_slice($_ARCHON->getAllSubjectsforJSON(),$start-1,100,true));
+    	} 
+    	else {
+            echo "batch_start Not found! Please enter a batch_start and resubmit the request.";
+    	}
+    }
+    else {
         echo "Authentication Failed";
     }
-} else {
-    echo "Please provide Username and Password";
+} 
+else {
+echo "Please provide Username and Password";
 }
-
-
 
 ?>
