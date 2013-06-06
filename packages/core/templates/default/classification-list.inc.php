73a6df0f684d763bd1e9a49f949535c6dd148c43
chrisprom <chris.prom@gmail.com>
2013-06-06 13:16:55 -0500

    added batch start and flattend JSON

--- packages/core/templates/default/classification-list.inc.php
+++ packages/core/templates/default/classification-list.inc.php
@@ -1,33 +1,28 @@
 <?php
 header('Content-Type: application/json');
-
 isset($_ARCHON) or die();
-
-//echo print_r($_ARCHON) ;
-//echo print_r($_ARCHON->getAllClassifications());
-
-$arrClassifications = $_ARCHON->loadTable("tblCollections_Classifications", "Classification", "ParentID, ClassificationIdentifier");
-
-
-
 if ($_REQUEST['apilogin'] && $_REQUEST['apipassword']) {
     if (!$_ARCHON->Security->verifyCredentials($_REQUEST['apilogin'], $_REQUEST['apipassword'])) {
         $_ARCHON->declareError("Authentication Failed");
     }
     if (!$_ARCHON->Error) {
 
-
-        echo json_encode(array_values($arrClassifications));
-
-
-
+      if ($_REQUEST['batch_start']){
+    		$start = ( $_REQUEST['batch_start'] < 1 ? 1: $_REQUEST['batch_start']);
+	
+			$arrClassifications = $_ARCHON->loadTable("tblCollections_Classifications", "Classification", "ID", NULL, NULL, NULL, false, NULL, false);
+    
+			$arrClassificationbatch = (array_slice($arrClassifications,$start-1,100,true));
+			
+        	echo json_encode(array_values($arrClassificationbatch));
+			}
+			else {
+				echo "batch_start Not found! Please enter a batch_start and resubmit the request."; 
+			}
     } else {
         echo "Authentication Failed";
     }
 } else {
     echo "Please provide Username and Password";
 }
-
-
-
 ?>
