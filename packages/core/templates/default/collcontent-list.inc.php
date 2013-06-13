6640c2be3791f36242351ac4c329c7697c5cf049
Randolph Taylor <mmindking@gmail.com>
2013-06-13 04:28:01 -0400

    Content  circular reference removed.

--- packages/core/templates/default/collcontent-list.inc.php
+++ packages/core/templates/default/collcontent-list.inc.php
@@ -44,47 +44,10 @@ function loadCollectionContent(){
 
 
         // get all root nodes
-        $arrObjects[] = $_ARCHON->getChildCollectionContent(0, $cid);
+       // $arrObjects[] = $_ARCHON->getChildCollectionContent(0, $cid);
 
         // traverse the path of nodes, adding all content at that level to the array
-        foreach($arrObjects as $obj)
-        {
-            // ignore the last node since we only need its siblings,
-            // unless a ghost node needs to be added, in which case the current key
-            // is actually the ghost's parent
-            if($obj != end($arrObjects) )
-            {
-                $arrObjects[] = $_ARCHON->getChildCollectionContent($obj->ID, $cid);
-            }
 
-              /*  // add a ghost node
-                if($obj == end($arrNodes))
-                {
-                    $objGhost = New CollectionContent(0);
-                    $objGhost->ParentID = $_REQUEST['pid'];
-                    $objGhost->ContainsContent = false;
-
-                    $lastArray = end($arrObjects);
-                    if(empty($lastArray))
-                    {
-                        array_pop($arrObjects);
-                        $arrObjects[] = array($objGhost);
-                    }
-                    else
-                    {
-
-                        $lastArray[0] = $objGhost;
-                        array_pop($arrObjects);
-                        $arrObjects[] = $lastArray;
-                    }
-
-                    //return currentkey back to 0
-                    $_REQUEST['currentkey'] = 0;
-               // }
-
-              */
-           // }
-        }
 
 
 
@@ -95,7 +58,13 @@ function loadCollectionContent(){
         // iterate through array of nodes in bottom up order*/
        // $arrObjects = array_reverse($arrObjects);
 
-    echo json_encode(array_values($arrObjects));
+    $objCollection = New Collection($_REQUEST['cid']);
+    //echo print_r($objCollection);
+    $objCollection->dbLoadContentjson();  //optional parameter limits to only one root node
+    $out = json_encode (array_values($objCollection->Content));
+    echo ($out);
+
+    //echo json_encode(array_values($arrObjects));
 
 
 
