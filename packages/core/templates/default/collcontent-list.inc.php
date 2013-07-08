c60381621e38d5d034901e957778b3e43e1fcb45
Randolph Taylor <mmindking@gmail.com>
2013-07-08 03:31:53 -0400

    Accession ,Content, DigitalContent
    accessions -> add primary creator like on collections
    collection content -> pull in subject and creator ID
    digital content -> add primary creator like on collections

--- packages/core/templates/default/collcontent-list.inc.php
+++ packages/core/templates/default/collcontent-list.inc.php
@@ -18,7 +18,39 @@ if ($_ARCHON->Security->Session->verifysession($session)){
             if ($_REQUEST['batch_start']){
                 $start = ( $_REQUEST['batch_start'] < 1 ? 1: $_REQUEST['batch_start']);
                 $arrout=loadCollectionContent($start);
-                 echo json_encode(RemoveBad($arrout));
+
+                //Creators
+                $arrCollectionContentCreator = getCollectionContentcreators();
+
+
+
+                foreach ($arrCollectionContentCreator as $CollectionContentRelatedObject)
+                {
+                    extract($CollectionContentRelatedObject);
+
+
+                    // Multi array  special process probably need to revisit
+
+                    if(isset($arrout['0'][$CollectionContentID])){
+
+                        $arrout['0'][$CollectionContentID]->Creators[] = $CollectionContentRelatedObject['CreatorID'];
+
+                    }
+                }
+
+                //Subjects
+                $arrCollectionContentSubjects= getCollectionContentSubjects();
+
+                foreach ($arrCollectionContentSubjects as $CollectionContentRelatedObject)
+                {
+                    extract($CollectionContentRelatedObject);
+                    if (isset($arrout['0'][$CollectionContentID])){
+
+                        $arrout['0'][$CollectionContentRelatedObject['CollectionContentID']]->Subjects[] = $CollectionContentRelatedObject['SubjectID'];
+
+                    }
+                }
+               echo json_encode(RemoveBad($arrout));
 
             }
             else
@@ -46,6 +78,8 @@ function loadCollectionContent($start){
     $cid = $_REQUEST['cid'];
 
 
+
+
     $objCollection = New Collection($_REQUEST['cid']);
     //echo print_r($objCollection);
     $objCollection->dbLoadContentjson();  //optional parameter limits to only one root node
@@ -53,7 +87,7 @@ function loadCollectionContent($start){
     $arrContent[]= array_slice($objCollection->Content,$start-1,100,true);
     $arrDisplay[]= array();
 
-    foreach ($arrContent as $contentObj)
+    foreach ( $arrContent as $contentObj)
     {
         if ($contentObj['ParentID'] == 0)
         {// Top Node Loaded first
@@ -74,8 +108,58 @@ function loadCollectionContent($start){
         }
     }
 
-return array_values($arrDisplay);
+return ($arrDisplay);
 
+}
+function getCollectionContentcreators()
+{
+    global $_ARCHON;
+
+
+    $query = "SELECT CollectionContentID,CreatorID FROM tblCollections_CollectionContentCreatorIndex";
+    $result = $_ARCHON->mdb2->query($query);
+
+
+    if(PEAR::isError($result))
+    {
+        trigger_error($result->getMessage(), E_USER_ERROR);
+    }
+
+    while($row = $result->fetchRow())
+    {
+        $arrCollectionContentCreators [] = $row;
+
+    }
+
+    $result->free();
+
+    return $arrCollectionContentCreators;
+
+}
+
+function getCollectionContentSubjects()
+{
+    global $_ARCHON;
+
+
+    $query = "SELECT DISTINCT CollectionContentID,SubjectID FROM tblCollections_CollectionContentSubjectIndex";
+    $result = $_ARCHON->mdb2->query($query);
+
+
+    if(PEAR::isError($result))
+    {
+        trigger_error($result->getMessage(), E_USER_ERROR);
+    }
+
+    while($row = $result->fetchRow())
+    {
+        $arrCollectionContentSubjects [] = $row;
+
+    }
+
+    $result->free();
+
+    return $arrCollectionContentSubjects;
 }
 
 function RemoveBad($CollectionContent) {
@@ -87,21 +171,19 @@ function RemoveBad($CollectionContent) {
 
 function Removefield($item,$key){
     unset($item->Collection);
-    unset($item->LevelContainer);
+
     unset($item->Content);
-    unset($item->DigitalContent);
-    unset($item->ToStringFields);
-    
+
+
     if (isset($item->UserFields)){
          foreach ($item->UserFields as $UserField){
+
               unset($UserField->ContentID);
-              unset($UserField->ToStringFields);
-              unset($UserField->Content);	     
-              $UserField->NoteType = $UserField->EADElement->EADTag;
-        	  unset($UserField->EADElementID);
-        	  unset($UserField->EADElement);
          }
+
     }
+
+
 }
 
 
