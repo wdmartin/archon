6121def7c41ee88bae4ae6e8fe07fb9475c5a159
chrisprom <chris.prom@gmail.com>
2013-08-28 17:04:21 -0500

    Fix creator realtionship pagination and creator relationships

--- packages/core/templates/default/creators-list.inc.php
+++ packages/core/templates/default/creators-list.inc.php
@@ -9,47 +9,40 @@ if ($_ARCHON->Security->Session->verifysession($session)){
    if (isset($_REQUEST['batch_start'])){
 
             //Handles the zero condition
+
             $start = ( $_REQUEST['batch_start'] < 1 ? 1: $_REQUEST['batch_start']);
-
-            $arrCreatorsrelated = getrelatedcreators();
-
-            // pulls Batches of 100 across
-
-           $arrCreators =array_slice($_ARCHON->getAllCreators(),$start-1,100,true);
+         
+           $arrCreators =array_slice($_ARCHON->getAllCreatorsJSON(),$start-1,100,true);
 		   header('HTTP/1.0 200 Created');				
 				if (empty($arrCreators)) {
 					exit ("No matching record(s) found for batch_start=".$_REQUEST['batch_start']);
 				}
-		   
-           // $arrCreators = $_ARCHON->getAllCreators();
-				//echo print_R($arrCreators);
-            foreach ($arrCreatorsrelated as $creatrel)
-             {
-                 $arrcreaterel = array($creatrel['RelatedCreatorID']=> $creatrel['CreatorRelationshipTypeID']);
-                $arrCreators[$creatrel['CreatorID']]->CreatorRelationships[] = $arrcreaterel;
-
-            }
+		    
+            array_walk($arrCreators, 'GetRelatedCreators');
 			array_walk($arrCreators,'Removefield');
-            echo json_encode(array_slice(RemoveBad($arrCreators),$start-1,100,true));
-
-        }else{
+         
+			echo json_encode($arrCreators);
+        }
+        else
+        {
 			header('HTTP/1.0 400 Bad Request');
             echo "batch_start Not found! Please enter a batch_start and resubmit the request.";
         }
-} else {
+} 
+else 
+{
 		header('HTTP/1.0 400 Bad Request');
         echo "Please submit your admin credentials to p=core/authenticate";
 }
 
 //FUNCTIONS
-function getrelatedcreators()
+
+function getRelatedCreators($item)
 {
     global $_ARCHON;
 
-
-        $query = "SELECT CreatorID,RelatedCreatorID,CreatorRelationshipTypeID FROM tblCreators_CreatorCreatorIndex";
+        $query = "SELECT RelatedCreatorID,CreatorRelationshipTypeID FROM tblCreators_CreatorCreatorIndex WHERE CreatorID=". $item->ID;
         $result = $_ARCHON->mdb2->query($query);
-
 
         if(PEAR::isError($result))
         {
@@ -59,19 +52,12 @@ function getrelatedcreators()
         while($row = $result->fetchRow())
         {
             $arrCreatorsrelated [] = $row;
-
         }
 
         $result->free();
-        $result->free();
-        return $arrCreatorsrelated;
+        $item->CreatorRelationships = $arrCreatorsrelated;
+}
 
-}
-function RemoveBad($Creators) {
-    
-	array_walk($Creators, 'Removefield');		
-    return $Creators;
-}
 
 function Removefield($item,$key){
 	unset($item->LanguageID);
@@ -87,6 +73,5 @@ function Removefield($item,$key){
 	unset($item->DigitalContent);
 	unset($item->Language);
 	unset($item->Creators);
-
 }
 ?>
