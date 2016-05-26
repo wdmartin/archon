87d3cc96264f3b70dea30d7fee2a9f6d367e376b
chrisprom <chris.prom@gmail.com>
2016-05-26 09:52:25 -0500

    fixing toString error on browse

--- packages/digitallibrary/templates/illinois/index.php
+++ packages/digitallibrary/templates/illinois/index.php
@@ -2,4 +2,6 @@
 $TemplateIndex['DigitalContent'] = "digitalcontent.inc.php";
 $TemplateIndex['DigitalContentList'] = "digitalcontentlist.inc.php";
 $TemplateIndex['Thumbnails'] = "thumbnails.inc.php";
+$TemplateIndex['DigitalNav'] = "digital-nav.inc.php";
+$TemplateIndex['Request'] = "request.inc.php";
 ?>
